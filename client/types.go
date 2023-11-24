7844a37df117ed3571a5ac888fe2ea4fbeb6d598
Paul Holden <paul@holden.dev>
2023-11-24 21:40:50 +0000

    Update sliceAsBytesSlice so it can be used for arbitrary element types.

--- client/types.go
+++ client/types.go
@@ -13,18 +13,15 @@ var (
 	float32ArrayCtor = js.Global().Get("Float32Array")
 )
 
-type numeric interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~float32 | ~float64
-}
-
 // sliceAsBytesSlice reinterprets the provided slice of data as a []byte.
 // See https://github.com/golang/go/issues/32402.
-func sliceAsBytesSlice[T numeric](data []T) []byte {
+func sliceAsBytesSlice[T any](data []T) []byte {
 	if len(data) == 0 {
 		return nil
 	}
+	var zero T
 	bytePtr := (*byte)(unsafe.Pointer(&data[0]))
-	byteLen := len(data) * int(unsafe.Sizeof(T(0)))
+	byteLen := len(data) * int(unsafe.Sizeof(zero))
 	bytes := unsafe.Slice(bytePtr, byteLen)
 	runtime.KeepAlive(data)
 	return bytes
