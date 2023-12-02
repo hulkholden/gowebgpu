39fc7747cfabaadc146df2c2b5884ac32ea0aa83
Paul Holden <paul@holden.dev>
2023-12-02 12:22:41 +0000

    Implement UniformBuffer using MappedAtCreation.

--- client/engine/uniform_buffer.go
+++ client/engine/uniform_buffer.go
@@ -1,8 +1,10 @@
 package engine
 
 import (
+	"syscall/js"
 	"unsafe"
 
+	"github.com/mokiat/gog/opt"
 	"github.com/mokiat/wasmgpu"
 )
 
@@ -11,28 +13,31 @@ type UniformBuffer struct {
 	buffer wasmgpu.GPUBuffer
 }
 
-func InitUniformBuffer[T any](device wasmgpu.GPUDevice, values T, opts ...BufferOption) UniformBuffer {
+func InitUniformBuffer[T any](device wasmgpu.GPUDevice, value T, opts ...BufferOption) UniformBuffer {
 	// TODO: use Struct to get this?
-	byteLen := unsafe.Sizeof(values)
+	byteLen := unsafe.Sizeof(value)
 
 	desc := wasmgpu.GPUBufferDescriptor{
-		Size:  wasmgpu.GPUSize64(byteLen),
-		Usage: wasmgpu.GPUBufferUsageFlagsUniform,
+		Size:             wasmgpu.GPUSize64(byteLen),
+		Usage:            wasmgpu.GPUBufferUsageFlagsUniform,
+		MappedAtCreation: opt.V(true),
 	}
 	for _, opt := range opts {
 		opt(&desc)
 	}
 
 	buffer := device.CreateBuffer(desc)
-	b := UniformBuffer{
+	js.CopyBytesToJS(uint8ArrayCtor.New(buffer.GetMappedRange(0, 0)), structAsByteSlice(value))
+	buffer.Unmap()
+
+	return UniformBuffer{
 		device: device,
 		buffer: buffer,
 	}
-	b.updateBuffer(structAsByteSlice(values))
-	return b
 }
 
-func (b UniformBuffer) updateBuffer(bytes []byte) {
+// TODO: make UniformBuffer generic so we can use `value T` here?
+func (b UniformBuffer) UpdateBuffer(bytes []byte) {
 	b.device.Queue().WriteBuffer(b.buffer, 0, bytes)
 }
 
